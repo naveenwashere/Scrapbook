89e760f952da0bf82d5864da61d8c218d59445e4
Naveen Kumar K <naveenwashere@gmail.com>
2016-03-05 11:57:06 +0530

    Adding logic using recursion
    Output: 
    
    Prime numbers - Approach One: 
    2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
    Prime numbers - Approach Divided by 2: 
    2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 
    Prime numbers - Approach Recursion: 
    2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -38,12 +38,44 @@ public class PrimeNumbers {
 		}
 	}
 	
+	public void primeNumsUsingRecursion(int n) {
+		if(n < 1) {
+			System.out.println("Enter a number greater than 1!");
+		}
+		for(int i = 2; i <= n; i++) {
+			boolean isPrime = findPrime(i, i/2, 0);
+			if(isPrime) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+	
+	/**
+	 * Recursive method to find Prime number
+	 * @param i the actual number to determine if prime
+	 * @param j the number in decreasing order from i/2 to 1, which forms the divisor
+	 * @param k the stopping condition where k = i/2
+	 * @return true / false based on whether the number is prime or not
+	 */
+	private boolean findPrime(int i, int j, int k) {
+		boolean isPrime = true;
+		if(j != k) {
+			if(i % j == 0 && (j != 1 && j != i)) {
+				isPrime = false;
+			}
+			return isPrime && findPrime(i, j - 1, k);
+		}
+		return isPrime;
+	}
+
 	public static void main(String[] args) {
 		PrimeNumbers pn = new PrimeNumbers();
 		System.out.println("Prime numbers - Approach One: ");
 		pn.printPrimeNumbersOne(100);
 		System.out.println("\nPrime numbers - Approach Divided by 2: ");
 		pn.printPrimeNumbersTwo(100);
+		System.out.println("\nPrime numbers - Approach Recursion: ");
+		pn.primeNumsUsingRecursion(100);
 	}
 
 }
