a5afff9ae7b7b87713eb55062c084be73705b633
Naveen Kumar K <naveenwashere@gmail.com>
2016-03-05 22:23:18 +0530

    Finding a Palindrome using Recursion

---
+++ PalindromeRecursion.java
@@ -0,0 +1,31 @@
+
+public class PalindromeRecursion {
+
+	public boolean isPalindrome(String str) {
+		return validatePalindrome(0, str.length(), str);
+	}
+	
+	private boolean validatePalindrome(int i, int length, String str) {
+		boolean isAMatch = true;
+		if((i+1) != length/2) {
+			isAMatch = validatePalindrome(i+1, length, str);
+		} 
+		
+		if (str.charAt(i) == str.charAt(length - 1 - i)) {
+			return isAMatch && true;
+		} else {
+			return isAMatch && false;
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "abiba";
+		PalindromeRecursion pr = new PalindromeRecursion();
+		if(pr.isPalindrome(str)) {
+			System.out.println("Is Palindrome"); 
+		} else {
+				System.out.println("Is not!");
+		}
+	}
+
+}
