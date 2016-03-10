4b1be3815d04eed4ab9ff8e72dfb7023e24930bb
Naveen Kumar K <naveenwashere@gmail.com>
2016-03-10 12:03:23 +0530

    Longest Palindromic Substring

---
+++ LongestPalindromicSubstring.java
@@ -0,0 +1,41 @@
+
+public class LongestPalindromicSubstring {
+	
+	static public String intermediatePalindrome(String s, int left, int right) {
+		if (left > right) {
+			return null;
+		}
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			left--;
+			right++;
+		}
+		//left + 1, because the check failed at 'left' value. right is taken as it is
+		//because toIndex for substring is exclusive
+		return s.substring(left + 1, right);
+	}
+
+	// O(n^2)
+	public static String longestPalindromeString(String s) {
+		if (s == null) {
+			return null;
+		}
+		String longest = s.substring(0, 1);
+		for (int i = 0; i < s.length() - 1; i++) {
+			// odd cases like 121
+			String palindrome = intermediatePalindrome(s, i, i);
+			if (palindrome.length() > longest.length()) {
+				longest = palindrome;
+			}
+			// even cases like 1221
+			palindrome = intermediatePalindrome(s, i, i + 1);
+			if (palindrome.length() > longest.length()) {
+				longest = palindrome;
+			}
+		}
+		return longest;
+	}
+
+	public static void main(String args[]) {
+		System.out.println(longestPalindromeString("forgeeksskeegfor"));
+	}
+}
