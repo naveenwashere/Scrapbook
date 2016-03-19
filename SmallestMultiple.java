39b6921487f7c3fe580ee5049ea073ee32ced783
Naveen Kumar K <naveenwashere@gmail.com>
2016-03-19 18:40:23 +0530

    Some optimizations

--- SmallestMultiple.java
+++ SmallestMultiple.java
@@ -1,5 +1,7 @@
 package com.euler;
 
+import java.util.Date;
+
 /**
  * https://projecteuler.net/problem=5
  * @author naveenkumarkalapati
@@ -9,10 +11,23 @@ public class SmallestMultiple {
 
 	public static void main(String[] args) {
 		SmallestMultiple sm = new SmallestMultiple();
-		sm.findSmallestMultiple(20);
+		Date start = new Date();
+		sm.findSmallestMultipleBruteForce(20);
+		Date end = new Date();
+		System.out.println((end.getTime() - start.getTime())/1000 + " seconds");
+		
+		start = new Date();
+		sm.findSmallestMultipleAnotherMinOptimized(20);
+		end = new Date();
+		System.out.println((end.getTime() - start.getTime())/1000 + " seconds");
+		
 	}
 
-	private void findSmallestMultiple(int till) {
+	/**
+	 * Takes 56 seconds
+	 * @param till
+	 */
+	private void findSmallestMultipleBruteForce(int till) {
 		boolean found = false;
 		long number = 0;
 		int counter = 0;
@@ -20,6 +35,33 @@ public class SmallestMultiple {
 			number++;
 			for(int i = 1; i <= till; i++) {
 				if(number % i == 0) {
+					counter++;
+				}
+			}
+			if(counter == till) {
+				found = true;
+				break;
+			} else {
+				counter = 0;
+			}
+		}
+		System.out.println(number);
+	}
+	
+	/**
+	 * Takes 6 seconds
+	 * @param till
+	 */
+	private void findSmallestMultipleAnotherMinOptimized(int till) {
+		boolean found = false;
+		long number = 0;
+		int counter = 0;
+		while(!found) {
+			number++;
+			for(int i = 1; i <= till; i++) {
+				if(number % i != 0) {
+					break;
+				} else {
 					counter++;
 				}
 			}
