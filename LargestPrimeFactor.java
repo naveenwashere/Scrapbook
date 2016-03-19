69e7fdfcfe14ca16972f530d56cd1dd5ac627407
Naveen Kumar K <naveenwashere@gmail.com>
2016-03-19 16:09:15 +0530

    Largest Prime Number Factor
    https://projecteuler.net/problem=3

---
+++ LargestPrimeFactor.java
@@ -0,0 +1,21 @@
+package com.euler;
+
+public class LargestPrimeFactor {
+
+	public static void main(String[] args) {
+		LargestPrimeFactor lpf = new LargestPrimeFactor();
+		System.out.println("Largest Prime Factor: " + lpf.findLargestPrimeFactor(600851475143L));
+	}
+
+	private long findLargestPrimeFactor(long n) {
+		long i = 0L;
+		for(i = 2; i < n; i++) {
+			if(n % i == 0) {
+				n = n/i;
+				i--;
+			}
+		}
+		return i;
+	}
+
+}
